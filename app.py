8f4f0c0259ef54a219c3b5869962b2b87e153f6f
Sanqn <sandisvert@gmail.com>
2022-05-29 19:52:14 +0300

    create table

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def live():
     if game.counter > 0:
         game.form_new_generation()
     game.counter += 1
-    return render_template("live.html", life=life)
+    return render_template("live.html", game=game)
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
